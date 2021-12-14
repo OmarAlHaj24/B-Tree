33f3bf1318c66fac2bb240aa5562c481f259964b
Omar Al Haj <omaralhaj24@gmail.com>
2021-12-14 15:32:29 +0200

    Created project

---
+++ cmake-build-debug/CMakeFiles/B_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "B_Tree.exe"
+  "B_Tree.exe.manifest"
+  "B_Tree.pdb"
+  "CMakeFiles/B_Tree.dir/main.cpp.obj"
+  "libB_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/B_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
