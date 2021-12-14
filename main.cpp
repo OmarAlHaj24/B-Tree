525924be79502a624e6592120acec7ec530f07a4
Omar Al Haj <omaralhaj24@gmail.com>
2021-12-14 19:35:35 +0200

    ..

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ class BTreeNode{
 private:
     int *keys;
     BTreeNode **children;
+    BTreeNode *parent;
     int currSize; //Number of keys
     int order; //Tree order
     bool leaf;
@@ -13,30 +14,39 @@ public:
     friend class BTree;
     BTreeNode(int order)
     {
+        parent = NULL;
         currSize = 0;
         this->order = order;
         leaf = true;
         keys = new int [order];
-        children = new BTreeNode* [order];
+        children = new BTreeNode* [order + 1];
     }
     ~BTreeNode()
     {
         delete [] keys;
         delete [] children;
     }
-    void splitNode(BTreeNode* root, BTreeNode* node)
+    void splitNode(BTreeNode* root)
     {
-//        while(!currNode->leaf){
-//            nodeToTraverse = currNode->currSize;
-//            for(int i = 0; i < currNode->currSize; i++){
-//                if(currNode->keys[i] > key){
-//                    nodeToTraverse = i;
-//                    break;
-//                }
-//            }
-//            currNode = currNode->children[nodeToTraverse];
-//        }
-        
+        if(this->currSize <= this->order - 1){
+            return;
+        }
+        int idx = this->currSize / 2;
+        BTreeNode* newNode = new BTreeNode(order);
+        for(int i = idx + 1, j = 0; i < this->currSize; i++, j++){
+            newNode->keys[j] = this->keys[i];
+        }
+        this->currSize = idx;
+        if(this->parent == NULL){
+            parent = new BTreeNode(order);
+            parent->children[0] = this;
+            root = parent;
+        }
+        parent->children[currSize + 1] = newNode;
+        parent->keys[parent->currSize] = keys[idx];
+        parent->currSize++;
+        newNode->parent = parent;
+        parent->splitNode(root);
     }
     void traverse()
     {
@@ -92,7 +102,7 @@ public:
             currNode->keys[currNode->currSize] = key;
             currNode->currSize++;
             sort(currNode->keys, currNode->keys + currNode->currSize);
-            currNode->splitNode(root, currNode);
+            currNode->splitNode(root);
         }
     }
     void Print()
