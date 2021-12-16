42ef7b57b122246725302482d958e28a187bb6c4
Ranaihab <rana.ihab7@gmail.com>
2021-12-16 11:20:48 +0200

    templates + travers function

--- main.cpp
+++ main.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
-#include <algorithm>
-using namespace std;
 
+using namespace std;
+/**
+ * Omar Khaled Al Haj 20190351
+ * Rana Ihab Ahmed 20190207
+ * Alaa Mahmoud Ebrahim 20190105
+ * */
+template<class T,int order>
+class BTree;
+
+template<class T,int order>
 class BTreeNode{
 private:
-    int *keys;
+    T *keys;
     BTreeNode **children;
     BTreeNode *parent;
     int currSize; //Number of keys
-    int order; //Tree order
     bool leaf;
 public:
-    friend class BTree;
-    BTreeNode(int order)
+    friend class BTree<T, order>;
+    BTreeNode()
     {
         parent = NULL;
         currSize = 0;
-        this->order = order;
         leaf = true;
-        keys = new int [order];
+        keys = new T [order];
         children = new BTreeNode* [order + 1];
-        for(int i = 0; i < order; i++){
-            keys[i] = NULL;
+        for(int i = 0; i < order; i++)
+        {
             children[i] = NULL;
         }
         children[order] = NULL;
@@ -29,42 +35,46 @@ public:
     ~BTreeNode()
     {
         delete [] keys;
-        for(int i = 0; i < order + 1; i++){
+        for(int i = 0; i < order + 1; i++)
+        {
             delete children[i];
         }
         delete [] children;
     }
     void splitNode(BTreeNode*& root);
-    void traverse()
+    void traverse(int level)
     {
+        for(int i = 0; i < level; i++)
+        {
+            cout<<" ";
+        }
         for(int i = 0; i < currSize; i++)
         {
-            cout << keys[i] << " ";
+            if(i != 0)
+            {
+                cout << ",";
+            }
+            cout << keys[i];
         }
         cout << endl;
         if(this->leaf) return;
         for(int i = 0; i < currSize + 1; i++)
         {
-            children[i]->traverse();
+            children[i]->traverse(level+2);
         }
     }
-    friend bool compareNodes(const BTreeNode* a, const BTreeNode* b);
+
 };
 
-bool compareNodes(const BTreeNode* a, const BTreeNode* b)
+template<class T,int order>
+void BTreeNode<T, order>::splitNode(BTreeNode<T, order> *&root)
 {
-    if(a->keys[0] < b->keys[0]) return true;
-    else return false;
-}
-
-void BTreeNode::splitNode(BTreeNode *&root)
-{
-    if(this->currSize <= this->order - 1)
+    if(this->currSize <= order - 1)
     {
         return;
     }
     int idx = this->currSize / 2;
-    BTreeNode* newNode = new BTreeNode(order);
+    BTreeNode* newNode = new BTreeNode();
     int i = idx + 1, j = 0;
     while(i < this->currSize)
     {
@@ -89,29 +99,40 @@ void BTreeNode::splitNode(BTreeNode *&root)
     this->currSize = idx;
     if(this->parent == NULL)
     {
-        parent = new BTreeNode(order);
+        parent = new BTreeNode();
         parent->children[0] = this;
         root = parent;
         root->leaf = false;
     }
-    parent->children[parent->currSize + 1] = newNode;
-    parent->keys[parent->currSize] = keys[idx];
+    for(int i = parent->currSize; i>=0; i--)
+    {
+        if(i == 0 || parent->keys[i-1]<=keys[idx])
+        {
+            parent->keys[i] = keys[idx];
+            parent->children[i+1] = newNode;
+            break;
+        }
+        else if(parent->keys[i-1]>keys[idx])
+        {
+            parent->keys[i] = parent->keys[i -1];
+            parent->children[i+1] = parent->children[i];
+        }
+    }
     parent->currSize++;
     newNode->parent = parent;
-    sort(parent->keys, parent->keys + parent->currSize);
-    sort(parent->children, parent->children + parent->currSize + 1, compareNodes);
     parent->splitNode(root);
 }
 
+template<class T,int order>
 class BTree{
 private:
-    BTreeNode* root; //The root of the tree
-    int order; //The degree of the tree
+    BTreeNode<T, order>* root; //The root of the tree
+    //int order; //The degree of the tree
 public:
-    BTree(int order)
+    BTree()
     {
         root = NULL;
-        this->order = order;
+        //this->order = order;
     }
     ~BTree()
     {
@@ -119,10 +140,10 @@ public:
     }
     void Insert(int key)
     {
-        BTreeNode* currNode = root;
+        BTreeNode<T, order>* currNode = root;
         if(root == NULL)
         {
-            root = new BTreeNode(order);
+            root = new BTreeNode<T, order>();
             root->keys[root->currSize] = key;
             root->currSize++;
         }
@@ -142,30 +163,88 @@ public:
                 }
                 currNode = currNode->children[nodeToTraverse];
             }
-            currNode->keys[currNode->currSize] = key;
+            for(int i = currNode->currSize; i>=1; i--)
+            {
+                if(currNode->keys[i-1]>key)
+                {
+                    currNode->keys[i] = currNode->keys[i -1];
+                    currNode->keys[i-1] = key;
+                }
+                else
+                {
+                    currNode->keys[i] = key;
+                    break;
+                }
+            }
             currNode->currSize++;
-            sort(currNode->keys, currNode->keys + currNode->currSize);
             currNode->splitNode(root);
         }
     }
     void Print()
     {
         if(root == NULL) return;
-        root->traverse();
+        root->traverse(0);
     }
 };
 
+
 int main() {
-    BTree t1(4);
+    // Construct a BTree of order 3, which stores int data
+    BTree<int, 3> t1;
+
     t1.Insert(1);
-    t1.Insert(2);
-    t1.Insert(3);
-    t1.Insert(4);
     t1.Insert(5);
-    t1.Insert(6);
-    t1.Insert(7);
-    t1.Insert(8);
-    t1.Insert(9);
-    t1.Insert(10);
-    t1.Print();
+    t1.Insert(0);
+    t1.Insert(4);
+    t1.Insert(3);
+    t1.Insert(2);
+
+    t1.Print(); // Should output the following on the screen:
+
+    /*
+    1,4
+      0
+      2,3
+      5
+    */
+
+    // Construct a BTree of order 5, which stores char data
+    BTree<char, 5> t;
+
+    // Look at the example in our lecture:
+    t.Insert('G');
+    t.Insert('I');
+    t.Insert('B');
+    t.Insert('J');
+    t.Insert('C');
+    t.Insert('A');
+    t.Insert('K');
+    t.Insert('E');
+    t.Insert('D');
+    t.Insert('S');
+    t.Insert('T');
+    t.Insert('R');
+    t.Insert('L');
+    t.Insert('F');
+    t.Insert('H');
+    t.Insert('M');
+    t.Insert('N');
+    t.Insert('P');
+    t.Insert('Q');
+
+    t.Print(); // Should output the following on the screen:
+
+    /*
+    K
+      C,G
+        A,B
+        D,E,F
+        H,I,J
+      N,R
+        L,M
+        P,Q
+        S,T
+    */
+
+    return 0;
 }
